57a8a192cd752274d7c25a2fb931cda00a25c7f5
Johannes-01 <54864647+Johannes-01@users.noreply.github.com>
2021-06-30 15:32:51 +0200

    fix

--- Server/Program.cs
+++ Server/Program.cs
@@ -101,7 +101,7 @@ namespace Server
             {
                 if (ip.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    if (ip.ToString().StartsWith("192.168.")) //192.168.60
+                    if (ip.ToString().StartsWith("192.168.60"))
                         return ip.ToString();
                 }
             }
